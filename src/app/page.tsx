4ac9ed1a96d45a0b598d49d746a5879d001e85bf
Chenxiu <fikrizeronine@gmail.com>
2025-12-13 07:56:02 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import LoadingScreen from "./loading";
 import MatrixRain from "@/components/MatrixRain";
 
 
@@ -12,7 +11,6 @@ export const revalidate = 60;
 export default async function Home() {
   return (
     <main className="min-h-screen relative">
-      {/* <LoadingScreen /> */}
       <Hero />
       <About />
       <Projects />
