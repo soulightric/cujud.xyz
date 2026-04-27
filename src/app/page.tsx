8511e714ef3338ddeee8880602a3434053977ab1
Cujud <165292882+soulightric@users.noreply.github.com>
2026-04-27 22:46:59 +0800

    Add MatrixRain component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export const revalidate = 60;
 export default async function Home() {
   return (
     <main className="min-h-screen relative">
+      <MatrixRain />
       <Navbar />
       <Hero />
       <About />
